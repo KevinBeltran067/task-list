bec22a55cd9a80db95b2c130ae399df8330e4d4b
Kevin <kevin.beltran067@hotmail.com>
2022-11-29 19:11:36 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import ToDoList from "./components/ToDoList";
+import useCreateTask from "./hooks/useCreateTask"
 
 function App() {
-  const tareas =[
-    {id: 1,
-    nombreTarea: "Curso",
-    estadoTarea: true},
-    {id: 2,
-    nombreTarea: "Ada",
-    estadoTarea: false},
-    {id: 3,
-    nombreTarea: "School",
-    estadoTarea: false},
-    {id: 4,
-      nombreTarea: "React",
-      estadoTarea: true},
-    {id: 5,
-      nombreTarea: "React",
-       estadoTarea: true}  
-  ]
+  const tareas = useCreateTask();
   return (<>
     <div className="todo-list-paper">
     <h2 className="todo-list-title">To Do List</h2>
---
+++ src/hooks/useCreateTask.jsx
@@ -0,0 +1,21 @@
+export default function useCreateTask(){
+    const tareas =[
+        {id: 1,
+        nombreTarea: "Curso",
+        estadoTarea: true},
+        {id: 2,
+        nombreTarea: "Ada",
+        estadoTarea: false},
+        {id: 3,
+        nombreTarea: "School",
+        estadoTarea: false},
+        {id: 4,
+          nombreTarea: "React",
+          estadoTarea: true},
+        {id: 5,
+          nombreTarea: "React",
+           estadoTarea: true}  
+      ]
+
+    return tareas;
+}
